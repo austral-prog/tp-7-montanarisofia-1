54e47448c4ef04ba9246123cc5a1e48541cd1c66
montanarisofia <162723443+montanarisofia@users.noreply.github.com>
2024-10-07 21:24:07 -0300

    Add files via upload

--- loops.py
+++ loops.py
@@ -1,18 +1,39 @@
 def index_of_by_index(word, list, index):
+    for i in range(index, len(list)):
+        if list[i] == word:
+            return i
     return -1
 
 
+
 def index_of_empty(list):
+    for i in range(len(list)):
+        if list[i] == "":
+            return i
     return -1
 
 
 def index_of(word, list):
+    for i in range(len(list)):
+        if list[i] == word:
+            return i
     return -1
+print(index_of("Black", ["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]))
 
 
 def put(word, list):
+    for i in range(len(list)):
+        if list[i] == "":
+            list[i] = word
+            return i
     return -1
 
 
 def remove(word, list):
+    count = 0
+    for i in range(len(list)):
+        if list[i] == word:
+            list[i] = ""
+            count += 1
+    return count
     return -1
--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,25 @@
 def enumerate_list(list):
-    return list
+    list2=[]
+    list3=[]
+    for item in list:
+        if item != "":
+            list2.append(item)
+    for i, items in enumerate(list2):
+        list3.append(f"{i}. {items}")
+    return list3
+
+print(enumerate_list(["Red", "Green", "", "White", "Black"]))
 
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(strings):
+    list1=[]
+    list2=[]
+    for str1 in strings:
+        if str1 != "":
+            list1.append(str1)
+    for index, str2 in enumerate(list1):
+        list2.append(f"{index}. {str2[::-1]}")
+    return list2
+
+print(enumerate_backwards(["Red", "Green", "", "White", "Black"]))
+
